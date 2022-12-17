9de2381f4c9f602ef628de59971557411e84c4be
dinho <dinho-16@hotmail.com>
2022-12-17 00:22:58 -0300

    Exercício 1 OK.

---
+++ src/main/java/br/com/Teste.java
@@ -0,0 +1,18 @@
+package br.com;
+
+import br.com.ex1.Ex1;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Teste {
+    public static void main(String[] args) throws IOException {
+
+        //Exercício 1
+        Ex1 e = new Ex1();
+        e.teste();
+    }
+
+
+}
---
+++ src/main/java/br/com/ex1/Ex1.java
@@ -0,0 +1,37 @@
+package br.com.ex1;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Ex1 {
+
+    public void teste() {
+
+        //Exercício 1
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("DIGITE O NÚMERO DE LINHAS QUE VOCÊ DESEJA: \n");
+
+        StringTokenizer token;
+        int linhas = Integer.parseInt(scan.nextLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+        System.out.println("INFORME OS NÚMEROS");
+        for (int c = 0; c < linhas; c++) {
+            token = new StringTokenizer(scan.nextLine());
+            arr.add(Integer.parseInt(token.nextToken()));
+        }
+        var par = arr.stream().filter(l -> l % 2 == 0).sorted().collect(Collectors.toList());
+        var impar = arr.stream().filter(l -> l % 2 != 0).sorted().collect(Collectors.toList());
+
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
+        result.addAll(Collections.singleton(par));
+        result.addAll(Collections.singleton(impar));
+        for (List<Integer> item : result
+        ) {
+            System.out.print(item);
+        }
+
+    }
+
+}
