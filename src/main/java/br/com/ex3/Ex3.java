3b643688f0b54dc5e2b773d7dd07b1fb60c8fe12
dinho <dinho-16@hotmail.com>
2022-12-18 23:55:15 -0300

    Exercício 2, 3, 4.

--- src/main/java/br/com/ex3/Ex3.java
+++ src/main/java/br/com/ex3/Ex3.java
@@ -11,42 +11,39 @@ public class Ex3 {
         System.out.println("Digite um numero VALOR ALVO:");
 
         Scanner alvo = new Scanner(System.in);
-        int a = alvo.nextInt();
+        float a = alvo.nextFloat();
         if (a == 0) {
             System.out.println(" Não pode ser vazio, escolha um numero");
         }
+        int[] vetor = {3};
+
 
         ArrayList<Integer> inteiros = new ArrayList<Integer>();
-
         ArrayList<Integer> pares = new ArrayList<Integer>();
-//        ArrayList<Integer> impares = new ArrayList<Integer>();
+        ArrayList<Integer> impares = new ArrayList<Integer>();
 
         System.out.println("Preencha a lista com números");
 
-
-        int contador = 0;
-
-//        while (contador < a) {
+        //int contador = 0;
+        // while
         for (int c = 0; c < a; c++) {
             Scanner scan2 = new Scanner(System.in);
 
-//            contador++;
             inteiros.add(scan2.nextInt());
-            if (inteiros.get(c) % 2 == a) {
-                pares.add(inteiros.get(contador));
+            if (inteiros.get(c) % 2 == 0) {
+                pares.add(inteiros.get(c));
             }
-//        }
-//        Collections.sort(pares);
-//        Collections.sort(impares);
-//        Collections.reverse(impares);
-
-            System.out.println("NUMEROS PARES: \n");
-            for (int numerosPares : pares
-            ) {
-                System.out.println(numerosPares);
-            }
-
-            alvo.close();
         }
+        Collections.sort(pares);
+        Collections.reverse(impares);
+
+        System.out.println("NUMEROS PARES: \n");
+        for (int numerosPares : pares
+        ) {
+            System.out.println(numerosPares);
+        }
+
+        alvo.close();
     }
 }
+
--- src/main/java/br/com/ex4/Ex4.java
+++ src/main/java/br/com/ex4/Ex4.java
@@ -6,17 +6,18 @@ public class Ex4 {
 
     public void teste4() {
 
-
         Scanner scan = new Scanner(System.in);
-
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Número de frases a desembaralhar");
         int a = scan.nextInt();
+        System.out.println("Insira a frase");
         for (int contador = 0; contador < a; contador++) {
-            String frase = scan.nextLine();
+            String frase = scan2.nextLine();
 
             StringBuilder fraseReversa = new StringBuilder(frase.substring(0, frase.length() / 2));
             StringBuilder fraseNormal = new StringBuilder(frase.substring(frase.length() / 2));
             fraseReversa.reverse();
-            fraseReversa.reverse();
+            fraseNormal.reverse();
             System.out.println(fraseReversa.toString() + fraseNormal.toString());
         }
     }
